3f8f3a2fc0fda8bc3ac5564d08301e262999828d
kiash <kiash.harilall@gmail.com>
2024-02-06 20:32:40 +0200

    file and folder structure

---
+++ src/components/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from "@mui/styles";
+
+export default makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        height: '100%',
+
+    },
+    toolbar: {
+        height: '70%',
+    },
+    content: {
+        flexGrow: '1',
+        padding: '2em'
+    }
+}));
